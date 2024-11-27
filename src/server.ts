76526fe8225b4944b8a60ab6bf0c3581fd805235
Allan Cassio <allan.cassio1@gmail.com>
2024-11-27 13:06:54 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,28 @@
-import "dotenv/config";
-import express from "express";
-import https from "https";
-import fs from "fs";
-import { router } from "./routes";
-import "dotenv/config";
-import cors from "cors";
+import 'dotenv/config'
+import express from 'express'
+import https from 'https'
+import fs from 'fs'
+import { router } from './routes'
+import 'dotenv/config'
+import cors from 'cors'
 // App
-const app = express();
+const app = express()
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use(router);
+app.use(cors())
+app.use(express.json())
+app.use(router)
 
 // Set port
-const port: string | number = process.env.PORT || "3333";
-
+const port = '4242'
 // Server
-app.listen(port, () => console.log(`Server running on localhost:${port}`));
+app.listen(port, () => console.log(`Server running on localhost:${port}`))
 
 const options: any = {
   cert: fs.readFileSync(`${__dirname}/keys/code.crt`),
   key: fs.readFileSync(`${__dirname}/keys/code.key`),
-};
+}
 
-https
-  .createServer(
-    options,
-    //    (req, res) => {
-    //   res.writeHead(200);
-    //   res.end("Hello, HTTPS!");
-    // }
-    app
-  )
-  .listen(3001, () => {
-    console.log("Server running on https://localhost:443");
-  });
+https.createServer(options, app).listen(3001, () => {
+  console.log('Server running on https://localhost:443')
+})
